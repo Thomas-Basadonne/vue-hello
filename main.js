1156353198e76488fd9b142d88b37da03e8938b1
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-10 09:44:48 +0100

    img

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const app = Vue.createApp({
   data() {
     return {
       message: "Ciao Vue 3!",
+      image: " ./img/planning1.png",
     };
   },
 });
 
-app.mount("#h1");
+app.mount("#root");
